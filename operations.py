c4bc2543f01610add6a2318dbefb7b3eaa219107
Maxwell <138258547+maxwellabgit@users.noreply.github.com>
2023-11-08 11:46:30 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,42 +1,77 @@
 import pandas as pd
 import numpy as np
-import re
 import time
+import random as rand
+
+
+#Database setup
+column = rand.choices(['Dropout' if i == 13 or i ==21 else i for i in range(0,100)],k=50000)
+othercolumn = rand.choices([i for i in range(20,80)], k=50000)
+colors = rand.choices(['Red', 'Blue', 'Green', 'Yellow', 'Violet'], k=50000)
+df_og = pd.Dataframe({'column':column, 'othercolumn':othercolumn, 'color':colors})
 
 #non-vectorized implementation:
+df = df_og
+start = time.time()
 newcolumn = []
 i = 0
 while i < len(df):
-  if df['column'][i] != None & df['column'][i] > 50:
-    y = 100
+  if df['column'][i] != 'Dropout' and df['othercolumn'][i] >= 50:
+    newcolumn.append(100)
+  elif df['column'][i] != 'Dropout' and df['othercolumn'][i] < 50:
+    newcolumn.append(0)
   else:
-    y = df['othercolumn'][i]
-  newcolumn.append(y)
+    newcolumn.append(df['othercolumn'][i])
   i += 1
+df = pd.DataFrame({'column':column, 'othercolumn':othercolumn, 'color':colors, 'newcolumn':newcolumn})
 
-#apply lambda, the good old 
-df['newcolumn'] = df.apply(lambda x: 100 if x['column'] not None and x['column'] > 50 else x['othercolumn'])
+t = time.time() - start
+print('while loop solution run time: ' + str(t))
 
-#np.where, the lightning fast bretheren of apply that vectorizes an operation on a column but only works with one logic statement
 
-df['newcolumn'] = np.where((df['column'] != None) & (df['column'] > 50), 100, df['othercolumn'])
+#define the above code as a function and apply lambda, the good old
+df = df_og
+start = time.time()
+def newcolumn(col1, col2):
+  if col1 != 'Dropout' and col2 >= 50:
+    return 100
+  elif col1 != 'Dropout' and col2 < 50:
+    return 0
+  else:
+    return col2
+df['newcolumn'] = df.apply(lambda x: newcolumn(x['column'], x['othercolumn']), axis=1)
+
+t2 = time.time() - start
+print('apply lambda solution run time: ' + str(t2))
+print('increase in speed from while loops: ' + str(int((t1 - t2)/t*100)) + '%')
+
+
+#np.where, the lightning fast bretheren of apply. It only works with one logic statement though :(
+df = df_og
+start = time.time()
+df['newcolumn'] = np.where((df['column']!='Dropout') & (df['othercolumn']>50), 100, df['othercolumn'])
+df['newcolumn'] = np.where((df['column']!='Dropout') & (df['othercolumn']<=50), 0, df['othercolumn'])
+
+t3 = time.time() - start
+print('numpy where solution run time: ' + str(t3))
+print('increase in speed from apply lambda: ' + str(int((t2 - t3)/t*100)) + '%')
+
 
 #np.select, vectorized and works with multiple logic statements (one giant if, elif, else)
-
+df = df_og
+start = time.time()
 conditions = [
-  (df['column'] != None) & (df['column'] > 50),
-  (df['column'] != None) & (df['column'] == 50),
-  (df['column'] != None) & (df['column'] < 50)
+  (df['column'] != 'Dropout') & (df['othercolumn'] > 50),
+  (df['column'] != 'Dropout') & (df['othercolumn'] <= 50)
 ]
 options = [
   100,
-  50,
   0
 ]
 df['newcolumn'] = np.select(conditions, options, df['othercolumn'])
 
-#quick way to filter certain columns or the entire database
+t4 = time.time() - start
+print('numpy select run time: ' + str(t4))
 
-df['column'] = df[df['column'].str.contains("|".join(wordstokeep))]
-df = df[df['othercolumn'] != df['newcolumn']]
+
 
